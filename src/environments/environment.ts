425c266848e24d333641cc226b30a6a2bda6ed76
NithinShivadas <nithinsivadas1710@gmail.com>
2026-04-03 18:11:32 +0530

    api url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // apiUrl: 'http://veztaapi.diligenzit.com/api/',
   // apiUrl: 'http://markveztaapi.diligenzit.com/api/',
-  // apiUrl: 'http://veztafinanceapi.diligenzit.com/api/',
+  apiUrl: 'http://veztafinanceapi.diligenzit.com/api/',
 };
