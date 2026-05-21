98979bf86fd123504e96a5f3129274d32c39f3ce
Sneha Sundaran Mp <sneha@renazentit.com>
2026-05-21 09:33:53 +0530

    changes in list misc receipt

--- src/app/pages/ACCOUNTS/list-misc-receipt/list-misc-receipt.component.ts
+++ src/app/pages/ACCOUNTS/list-misc-receipt/list-misc-receipt.component.ts
@@ -383,7 +383,6 @@ export class ListMiscReceiptComponent {
         }
         else if (this.selectedmiscellaneousData.TRANS_STATUS == 2) {
           this.PopupTitle = 'View Miscellaneous Receipt '
-
         }
         else {
           this.PopupTitle = 'Verify Miscellaneous Receipt'
@@ -401,25 +400,18 @@ export class ListMiscReceiptComponent {
   // ============================Approve Popup function=========================================
   onApproveClick(e: any): void {
     e.cancel = true;
-
     const transStatus = e.row.data.TRANS_STATUS;
     const id = e.row.data.TRANS_ID;
-
     this.statusFinder = transStatus;
-
     // Clear stale data
     this.selectedmiscellaneousData = null;
     this.verifypopup = false;
-
     this.dataService.selectMiscReceipt(id).subscribe({
       next: (res: any) => {
         console.log(res);
-
         // assign fresh object reference
         this.selectedmiscellaneousData = { ...res.Data };
-
         this.isReadOnlyPayment = transStatus === 'Approve';
-
         // open popup only after data is ready
         this.verifypopup = true;
       },
@@ -438,9 +430,7 @@ export class ListMiscReceiptComponent {
 
   toggleFilters() {
     this.isFilterOpened = !this.isFilterOpened;
-
     const grid = this.dataGrid?.instance; // Assuming you have @ViewChild('dataGrid') dataGrid: DxDataGridComponent;
-
     if (grid) {
       grid.option('filterRow.visible', this.isFilterOpened);
       grid.option('headerFilter.visible', this.isFilterOpened);
