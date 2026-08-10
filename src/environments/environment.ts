3c021cf9d2d380fb914463e0b31e9d8b024921cc
anusree-kr <anusree@renazentit.com>
2026-08-10 15:01:35 +0530

    item property ongoing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://markveztaapi.diligenzit.com/api/',
-  // apiUrl: 'http://veztafinanceapi.diligenzit.com/api/',
+  // apiUrl: 'http://markveztaapi.diligenzit.com/api/',
+  apiUrl: 'http://veztafinanceapi.diligenzit.com/api/',
   // apiUrl: 'http://veztaapi.diligenzit.com/api/',
   // apiUrl: 'https://localhost:7215/api/',
   // apiUrl: 'https://dmgtapi.veztaonline.com/api/',
   // apiUrl: 'http://financeapi.advanceworldgroup.net/api/'
-  apiUrl: 'http://veztaapi.diligenzit.com/api/',
+  // apiUrl: 'http://veztaapi.diligenzit.com/api/',
 };
