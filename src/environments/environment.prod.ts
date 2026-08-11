9feb0c3a4bd5005175234b38ea495d684c7d2e60
anusree-kr <anusree@renazentit.com>
2026-08-11 17:38:21 +0530

    styling appllied in account related modules and reports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://localhost:44330/api/',
-  // apiUrl: 'http://markveztaapi.diligenzit.com/api/',
+  apiUrl: 'http://markveztaapi.diligenzit.com/api/',
   // apiUrl: 'http://veztafinanceapi.diligenzit.com/api/',
   // apiUrl: 'https://dmgtapi.veztaonline.com/api/',
   // apiUrl: 'http://financeapi.advanceworldgroup.net/api/',
-  apiUrl: 'http://veztaapi.diligenzit.com/api/',
+  // apiUrl: 'http://veztaapi.diligenzit.com/api/',
 };
